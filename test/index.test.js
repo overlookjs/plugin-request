b9070ca3084e88c1ac882a98a8d0a25e0eb59439
overlookmotel <theoverlookmotel@gmail.com>
2020-05-19 17:43:17 +0100

    Do not subclass Route [improve]
    Closes #1.

--- test/index.test.js
+++ test/index.test.js
@@ -7,6 +7,7 @@
 
 // Modules
 const Plugin = require('@overlook/plugin'),
+	Route = require('@overlook/route'),
 	requestPlugin = require('@overlook/plugin-request');
 
 // Init
@@ -17,5 +18,10 @@ require('./support/index.js');
 describe('plugin', () => {
 	it('is a Plugin', () => {
 		expect(requestPlugin).toBeInstanceOf(Plugin);
+	});
+
+	it('when passed to `Route.extend()`, returns Route unchanged', () => {
+		const RequestRoute = Route.extend(requestPlugin);
+		expect(RequestRoute).toBe(Route);
 	});
 });
